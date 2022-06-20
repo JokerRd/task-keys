d32b1c863503ac4f7dc950538c8d8097b1646616
Коньков Владислав <v.konkov@bftcom.com>
2022-06-21 00:30:11 +0500

    Решение задачи

--- src/Keys.tsx
+++ src/Keys.tsx
@@ -1,5 +1,61 @@
 import { IItem } from './index';
+import React, { useEffect, useState } from 'react';
 
 export function Keys(props: { initialData: IItem[]; sorting: 'ASC' | 'DESC' }) {
-    return <div></div>;
+    useEffect(() => {
+        if (props.sorting === 'DESC') {
+            props.initialData.sort(sortAsc);
+        } else {
+            props.initialData.sort(sortDesc);
+        }
+    }, [props.initialData, props.sorting]);
+    return (
+        <div>
+            <ol>
+                {props.initialData.map((item) => {
+                    return <ElementList key={item.id} item={item} />;
+                })}
+            </ol>
+        </div>
+    );
 }
+
+const ElementList = (props: { item: IItem }) => {
+    const [isEdited, setIsEdited] = useState(false);
+    const li = <li onClick={() => setIsEdited(true)}>{props.item.name}</li>;
+    const input = (
+        <input
+            onKeyDown={(e) => onKeyDown(e, props.item, setIsEdited)}
+            defaultValue={props.item.name}
+        ></input>
+    );
+    return isEdited ? input : li;
+};
+
+const onKeyDown = (
+    e: React.KeyboardEvent<HTMLInputElement>,
+    item: IItem,
+    setIsEdited: setIsEditedFunction,
+) => {
+    if (e.key === 'Enter') {
+        item.name = e.currentTarget.value;
+        setIsEdited(false);
+    } else if (e.key === 'Escape') {
+        setIsEdited(false);
+    }
+};
+
+function sortAsc(a: IItem, b: IItem): number {
+    if (a.name.toLowerCase() < b.name.toLowerCase()) {
+        return -1;
+    } else if (a.name.toLowerCase() > b.name.toLowerCase()) {
+        return 1;
+    }
+    return 0;
+}
+
+function sortDesc(a: IItem, b: IItem): number {
+    return sortAsc(b, a);
+}
+
+type setIsEditedFunction = (flag: boolean) => void;
